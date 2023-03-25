9447257b353d1b525e12a3636ecaa4027ef81b8b
Kazakov Kostya <leap74rus@gmail.com>
2023-03-25 09:03:34 +0300

    add: stop game

--- alien_invasion.py
+++ alien_invasion.py
@@ -36,9 +36,12 @@ class AlienInvasion:
         """Запуск основного цикла игры."""
         while True:
             self._check_events()
-            self.ship.update()
-            self._update_bullets()
-            self._update_aliens()
+
+            if self.stats.game_active:
+                self.ship.update()
+                self._update_bullets()
+                self._update_aliens()
+
             self._update_screen()
 
     def _check_events(self):
@@ -111,19 +114,22 @@ class AlienInvasion:
 
     def _ship_hit(self):
         """Обрабатывает столкновение коробля с пришельцами."""
-        # Уменьшает ships_left.
-        self.stats.ships_left -= 1
+        if self.stats.ships_left > 0:
+            # Уменьшает ships_left.
+            self.stats.ships_left -= 1
 
-        # Очистка списка пришельцев и снарядов.
-        self.aliens.empty()
-        self.bullets.empty()
+            # Очистка списка пришельцев и снарядов.
+            self.aliens.empty()
+            self.bullets.empty()
 
-        # Создание нового флота и размещение корабля по центру.
-        self._create_fleet()
-        self.ship.center_ship()
+            # Создание нового флота и размещение корабля по центру.
+            self._create_fleet()
+            self.ship.center_ship()
 
-        # Пауза.
-        sleep(0.5)
+            # Пауза.
+            sleep(0.5)
+        else:
+            self.stats.game_active = False
 
     def _create_fleet(self):
         """Создание флота вторжения."""
