9dfa466fdc16e9df12a351cbe6776b24860ed559
Kazakov Kostya <leap74rus@gmail.com>
2023-03-25 12:45:46 +0300

    fix: scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -36,14 +36,14 @@ class Scoreboard:
     def prep_high_score(self):
         """Преобразует рекордный счет в графическое изображение."""
         high_score = round(self.stats.high_score, -1)
-        high_score_str = "{:,}".format(high_score)
-        self.high_score_img = self.font.render(
-            high_score_str, True, self.text_color, self.ai_settings.bg_color
+        high_score_str = f"{high_score:,}"
+        self.high_score_image = self.font.render(
+            high_score_str, True, self.text_color, self.settings.bg_color
         )
 
         # Рекорд выравниваеться по центру верхней стороны.
-        self.high_score_rect = self.high_score_img.get_rect()
-        self.high_score_rect.centerx = self.score_rect.centerx
+        self.high_score_rect = self.high_score_image.get_rect()
+        self.high_score_rect.centerx = self.screen_rect.centerx
         self.high_score_rect.top = self.score_rect.top
 
     def check_high_score(self):
@@ -55,4 +55,4 @@ class Scoreboard:
     def show_score(self):
         """Выводит счет на экран."""
         self.screen.blit(self.score_image, self.score_rect)
-        self.screen.blit(self.high_score_img, self.high_score_rect)
+        self.screen.blit(self.high_score_image, self.high_score_rect)
